c84b5a61794ea30587072992649e780da5ce2cff
SanQiuHG <87588619+SanQiuHG@users.noreply.github.com>
2021-07-18 13:06:43 +0700

    Update Panggilan.php

--- Panggilan.php
+++ Panggilan.php
@@ -1,19 +1,19 @@
 <?php
-// Batasi 3x Telpon Setiap Satu Nomor
-fungsi  kirim ( $ telepon ){
-        $ ch = curl_init ();
-        curl_setopt ( $ ch , CURLOPT_URL , "https://www.tokocash.com/oauth/otp" );                      curl_setopt ( $ ch , CURLOPT_SSL_VERIFYPEER , false );
-        curl_setopt ( $ ch , CURLOPT_FOLLOWLOCATION , benar );
-        curl_setopt ( $ ch , CURLOPT_RETURNTRANSFER , benar );
-        curl_setopt ( $ ch , CURLOPT_HEADER , benar );
-        curl_setopt ( $ ch , CURLOPT_POST , 1 );
-        curl_setopt ( $ ch , CURLOPT_POSTFIELDS , "msisdn=$telepon&accept=panggilan" );                        $ asw = curl_exec ( $ ch );
-        curl_close ( $ ch );
-                gema  $ asw . "\n" ;
+// Limit 3x Telpon Setiap Satu Nomor
+function send($phone){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");                        $asw = curl_exec($ch);
+        curl_close($ch);
+                echo $asw."\n";
 }
-echo  "Copyright = SanQiu HG\n⏠⏡⏠⏡⏠⏡⏠⏡\n" ;
-echo  "Nomor\nMasukan : " ;
-$ nomor = trim ( fgets ( STDIN ));
-$ eksekusi = kirim ( $ nomor );
-cetak $ eksekusi ;
+echo "COPYRIGHT ; SGBTEAM\n\n";
+echo "Nomor\nInput : ";
+$nomor = trim(fgets(STDIN));
+$execute = send($nomor);
+print $execute;
 ?>
